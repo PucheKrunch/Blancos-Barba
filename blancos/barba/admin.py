be4503c4b6aed62ca292ed76f7d8de364d155b69
Santiago Benavides <santiago.benavides@alumnos.udg.mx>
2021-11-16 23:41:27 -0600

    Database Created

--- blancos/barba/admin.py
+++ blancos/barba/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Empleado)
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Iva)
+admin.site.register(Proveedor)
+admin.site.register(Venta)
+admin.site.register(DetalleVenta)
+admin.site.register(Compra)
+admin.site.register(DetalleCompra)
+admin.site.register(Baja)
+admin.site.register(DetalleBaja)
--- blancos/barba/models.py
+++ blancos/barba/models.py
@@ -1,3 +1,105 @@
 from django.db import models
 
 # Create your models here.
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=50,null=True,blank=True)
+    apellidos = models.CharField(max_length=50,null=True,blank=True)
+    sueldo = models.FloatField(null=True,blank=True)
+    cargo = models.CharField(max_length=50,null=True,blank=True)
+    direccion = models.CharField(max_length=50,null=True,blank=True)
+    status = models.CharField(max_length=50,null=True,blank=True)
+    password = models.CharField(max_length=50,null=True,blank=True)
+    telefono = models.CharField(max_length=50,null=True,blank=True)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellidos
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50,null=True,blank=True)
+    apellidos = models.CharField(max_length=50,null=True,blank=True)
+    correo = models.CharField(max_length=50,null=True,blank=True)
+    telefono = models.CharField(max_length=50,null=True,blank=True)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellidos
+
+class Iva(models.Model):
+    porcentaje = models.FloatField(null=True,blank=True)
+    fecha_aplicacion = models.DateField(null=True,blank=True)
+    fecha_termino = models.DateField(null=True,blank=True)
+
+    def __str__(self):
+        return str(self.porcentaje)
+
+class Producto(models.Model):
+    descripcion = models.CharField(max_length=50,null=True,blank=True)
+    precio = models.FloatField(null=True,blank=True)
+    color = models.CharField(max_length=50,null=True,blank=True)
+    talla = models.CharField(max_length=50,null=True,blank=True)
+    existencia = models.IntegerField(null=True,blank=True)
+
+    def __str__(self):
+        return self.descripcion
+
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=50,null=True,blank=True)
+    correo = models.CharField(max_length=50,null=True,blank=True)
+    telefono = models.CharField(max_length=50,null=True,blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Venta(models.Model):
+    fechaVenta = models.DateField(null=True,blank=True)
+    fechaEntrega = models.DateField(null=True,blank=True)
+    cliente = models.ForeignKey(Cliente,on_delete=models.SET_NULL,null=True,blank=True)
+    vendedor = models.ForeignKey(Empleado,on_delete=models.SET_NULL,null=True,blank=True)
+    repartidor = models.ForeignKey(Empleado,on_delete=models.SET_NULL,null=True,blank=True,related_name='repartidor')
+    direccion = models.CharField(max_length=50,null=True,blank=True)
+    status = models.CharField(max_length=50,null=True,blank=True)
+
+    def __str__(self):
+        return str(self.cliente.nombre) + ' ' + str(self.cliente.apellidos) + ' ' + str(self.id)
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta,on_delete=models.CASCADE,null=True,blank=True)
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE,null=True,blank=True)
+    cantidad = models.IntegerField(null=True,blank=True)
+    precio = models.FloatField(null=True,blank=True)
+
+    def __str__(self):
+        return str(self.venta.cliente.nombre) + ' ' + str(self.venta.cliente.apellidos) + ' ' + str(self.venta.id)
+
+class Compra(models.Model):
+    fechaCompra = models.DateField(null=True,blank=True)
+    proveedor = models.ForeignKey(Proveedor,on_delete=models.CASCADE,null=True,blank=True)
+    empleado = models.ForeignKey(Empleado,on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self):
+        return self.proveedor.nombre
+
+class DetalleCompra(models.Model):
+    compra = models.ForeignKey(Compra,on_delete=models.CASCADE,null=True,blank=True)
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE,null=True,blank=True)
+    cantidad = models.IntegerField(null=True,blank=True)
+    precio = models.FloatField(null=True,blank=True)
+
+    def __str__(self):
+        return self.compra.proveedor.nombre
+
+class Baja(models.Model):
+    fechaBaja = models.DateField(null=True,blank=True)
+    compra = models.ForeignKey(Compra,on_delete=models.CASCADE,null=True,blank=True)
+    empleado = models.ForeignKey(Empleado,on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self):
+        return str(self.fechaBaja)
+
+class DetalleBaja(models.Model):
+    baja = models.ForeignKey(Baja,on_delete=models.CASCADE,null=True,blank=True)
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE,null=True,blank=True)
+    cantidad = models.IntegerField(null=True,blank=True)
+    motivo = models.CharField(max_length=150,null=True,blank=True)
+
+    def __str__(self):
+        return str(self.baja.fechaBaja)
